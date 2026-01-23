f0b07bdd4e6148b99ecb32287d54542d20ca06d4
aniket <aniket@9766228627@gmail.com>
2026-01-23 11:49:36 +0530

    Added new Changes

--- src/main/java/com/ecommerce/order/entity/Order.java
+++ src/main/java/com/ecommerce/order/entity/Order.java
@@ -5,7 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import java.math.BigDecimal;
 import java.util.List;
 
 @Entity
@@ -19,7 +18,7 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String orderNumber;
-    
+
     @OneToMany(cascade = CascadeType.ALL)
     private List<OrderLineItems> orderLineItemsList;
    /// git status
